77f2716d3a681abd7793e803f9f3108875400920
Shun100 <denkidenki55@gmail.com>
2026-05-19 23:31:38 +0900

    WIP on auth features

--- src/modules/auth/auth.repository.ts
+++ src/modules/auth/auth.repository.ts
@@ -13,5 +13,27 @@ export const authRepository = {
     const result = await api.post('/auth/signup', { name, email, password });
     const { user, token } = result.data;
     return { user: new User(user), token};
+  },
+
+  /**
+   * ログイン
+   * @param { string } email 
+   * @param { string } password 
+   * @returns { Promise<{ user: User, token: string }> }
+   */
+  async signin(email: string, password: string): Promise<{ user: User, token: string }> {
+    const result = await api.post('/auth/signin', { email, password });
+    const { user, token } = result.data;
+    return { user: new User(user), token };
+  },
+
+  /**
+   * 現在のユーザ情報取得
+   * @returns { Promise<User> }
+   */
+  async getCurrentUser(): Promise<User> {
+    const result = await api.get('/current-user');
+    const { user } = result.data;
+    return new User(user);
   }
 }
